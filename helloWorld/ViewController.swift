8c92ebec8baf062375e9d0932f2f3152adc973ae
Jorge <honomx@gmail.com>
2015-08-05 23:04:04 -0700

    “message”

--- helloWorld/ViewController.swift
+++ helloWorld/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var nameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func helloWorldAction(sender: UITextField) {
+        nameLabel.text = "Hello \(sender.text)";
+    }
 
 }
 
